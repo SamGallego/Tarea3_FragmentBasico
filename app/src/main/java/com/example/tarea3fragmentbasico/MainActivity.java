32360a1e5ceea10f44ea693e0be0ab56b2022958
DAMDU212 <samuel.gallego15@gmail.com>
2024-12-10 16:38:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tarea3fragmentbasico/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.tarea3fragmentbasico;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextText;
+    private Button actualizarButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editTextText = findViewById(R.id.editTextText);
+        actualizarButton = findViewById(R.id.actualizar);
+
+
+        actualizarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtén el texto del EditText
+                String texto = editTextText.getText().toString();
+
+                // Crea una nueva instancia del fragmento con el texto como argumento
+                MiFragmento miFragmento = MiFragmento.newInstance(texto);
+
+                // Reemplaza el contenido del FragmentContainerView con el nuevo fragmento
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainerView, miFragmento);
+                fragmentTransaction.commit();
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tarea3fragmentbasico/MiFragmento.java
@@ -0,0 +1,76 @@
+package com.example.tarea3fragmentbasico;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MiFragmento#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MiFragmento extends Fragment {
+
+    private static final String ARG_TEXTO = "texto";
+
+    private String textoRecibido;
+    private TextView textFragment;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MiFragmento() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MiFragmento.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MiFragmento newInstance(String texto) {
+        MiFragmento fragment = new MiFragmento();
+        Bundle args = new Bundle();
+        args.putString(ARG_TEXTO, texto);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            textoRecibido = getArguments().getString(ARG_TEXTO);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_mi_fragmento, container, false);
+        textFragment = view.findViewById(R.id.textFragment);
+
+        // Establece el texto recibido en el TextView
+        if (textoRecibido != null) {
+            textFragment.setText(textoRecibido);
+        }
+
+        return view;
+    }
+}
